0a6dd59d4728838c286586c26dc0b3b91e15d553
YersonMv01 <yersonalexis50@gmail.com>
2025-01-03 18:25:43 -0500

    Ready for renderRRR

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -131,6 +131,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+if not DEBUG:    
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+   
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
